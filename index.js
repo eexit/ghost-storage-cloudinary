670ca37e8a2edb1ef9419c09bf7f5cf8c04be05a
Marco Mornati <ilmorna@gmail.com>
2017-07-25 13:54:25 +0200

    Updated module following the latests Ghost Storage specifications

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
+'use strict';
+
 var Promise = require('bluebird');
 var cloudinary = require('cloudinary');
 var util = require('util');
+var BaseAdapter = require('ghost-storage-base');
 
-var baseStore = require('ghost-storage-base');
+class CloudinaryAdapter extends BaseAdapter{
+  constructor(options) {
+    super(options);
+    this.config = options || {};
+    cloudinary.config(options);
+  }
 
-// TODO: Add support for private_cdn
-// TODO: Add support for secure_distribution
-// TODO: Add support for cname
-// TODO: Add support for cdn_subdomain
-// http://cloudinary.com/documentation/node_additional_topics#configuration_options
-
-function CloudinaryStore(config) {
-    baseStore.call(this);
-    this.config = config || {};
-    cloudinary.config(config);
-}
-
-util.inherits(CloudinaryStore, baseStore);
-
-CloudinaryStore.prototype.save = function(image) {
-  var secure = this.config.secure || false;
-
-  return new Promise(function(resolve) {
-    cloudinary.uploader.upload(image.path, function(result) {
-      resolve(secure ? result.secure_url : result.url);
+  exists(filename) {
+    return new Promise(function(resolve) {
+      if (cloudinary.image(filename, { })) {
+          resolve(true);
+      } else {
+          resolve(false);
+      }
     });
-  });
-};
+  }
 
-CloudinaryStore.prototype.delete = function(image) {
+  save(image, targetDir) {
+    var secure = this.config.secure || false;
 
-  return new Promise(function(resolve) {
-    cloudinary.uploader.destroy('zombie', function(result) {
-      resolve(result)
+    return new Promise(function(resolve) {
+      cloudinary.uploader.upload(image.path, function(result) {
+        resolve(secure ? result.secure_url : result.url);
+      });
     });
-  });
-};
+  }
 
-CloudinaryStore.prototype.exists = function(filename) {
-  return new Promise(function(resolve) {
-    if (cloudinary.image(filename, { })) {
-        resolve(true);
-    } else {
-        resolve(false);
+  serve() {
+    return function customServe(req, res, next) {
+      next();
     }
-  });
+  }
 
+  delete(image) {
+    return new Promise(function(resolve) {
+      cloudinary.uploader.destroy('zombie', function(result) {
+        resolve(result)
+      });
+    });
+  }
+
+  read() {
+    //Not used. The image is uploaded with the direct URL to the Cloudinary Service. No Need to pass through this plugin
+  }
 }
 
-CloudinaryStore.prototype.serve = function() {
-  return function (req, res, next) {
-    next();
-  };
-};
-
-module.exports = CloudinaryStore;
+module.exports = CloudinaryAdapter;
